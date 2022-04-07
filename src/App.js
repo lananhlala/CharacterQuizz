d7031b8f38edaa073d8da7d5300efcd617000bff
lananhlala <lananhlala31@gmail.com>
2022-04-07 16:45:35 +0700

    whole project

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import question_data from './api/questions';
+import answer_data from './api/answer';
+
+export default function App() {
+	const questions = question_data
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showResult, setShowResult] = useState(false);
+	const [points, setPoints] = useState({"A": 0, "B": 0, "C": 0, "D": 0, "A2": 0, "B2": 0, "C2": 0, "D2": 0});
+	const [result, setResult] = useState("");
+
+	const handleAnswerOptionClick = (answer_type) => {
+		setPoints(prevPoints => {
+			let res = {...prevPoints};
+			res[answer_type] = prevPoints[answer_type] + 1;
+			setResult(() => {
+				let totalPoints = {}
+				totalPoints["Red"] = points.A + points.D2;
+				totalPoints["Blue"] = points.B + points.C2;
+				totalPoints["Yellow"] = points.C + points.B2;
+				totalPoints["Green"] = points.D + points.A2;
+				let maxPointColor = "";
+				let maxPoint = -1
+				Object.entries(totalPoints).map(([key, value]) => {
+					if (maxPoint < value) {
+						maxPoint = value;
+						maxPointColor = key;
+					}
+					return maxPointColor;
+				})
+				switch(maxPointColor) {
+					case "Red": 
+						return answer_data["Red"];
+					case "Blue":
+						return answer_data["Blue"];
+					case "Yellow":
+						return answer_data["Yellow"];
+					case "Green":
+						return answer_data["Green"];
+					default:
+						return "Khong load duoc";
+				}
+			})
+			return res;
+		});
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowResult(true);
+			
+		}
+	};
+	return (
+		<div className='app'>
+			<h1 className='header'>TRẮC NGHIỆM TÂM LÝ: ĐÂU LÀ MÀU SẮC CHỦ ĐẠO TRONG TÍNH CÁCH CỦA BẠN?</h1>
+			<div className='legend'>Nguồn: Weibo Việt Name - Zhihu</div>
+			{showResult ? (
+				<div className='score-section'>
+					<span className='display-linebreak'>
+						Kết quả của bạn là: {result}
+					</span>
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Câu {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.answer_type)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
---
+++ src/api/questions.js
@@ -0,0 +1,273 @@
+var question_data =  [
+    {
+        questionText: 'Quan niệm sống của tôi là:',
+        answerOptions: [
+            { answerText: 'Tôi hy vọng có được thật nhiều trải nghiệm trong cuộc sống, vì vậy ý tưởng của tôi vô cùng đa dạng.', answer_type: 'A' },
+            { answerText: 'Cẩn thận đưa ra mục tiêu dựa trên sự hợp lý, một khi đã có mục tiêu thì kiên quyết thực hiện đến cùng.', answer_type: 'B' },
+            { answerText: 'Để tâm đến việc đạt được tất cả thành tựu trong khả năng của mình.', answer_type: 'C' },
+            { answerText: 'Không thích rủi ro, thích tận hưởng trạng thái ổn định.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Nếu tôi đi leo núi, khi xuống núi tôi thường sẽ:',
+        answerOptions: [
+            { answerText: 'Thích sự mới mẻ, sẽ tìm một tuyến đường mới để xuống núi', answer_type: 'A' },
+            { answerText: 'Thích an toàn và ổn định, sẽ quay về bằng đường cũ', answer_type: 'B' },
+            { answerText: 'Thích thử thách với khó khăn, sẽ tìm một tuyến đường mới để xuống núi', answer_type: 'C' },
+            { answerText: 'Thích dễ dàng thuận tiện, sẽ quay về bằng đường cũ', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Khi nói chuyện, tôi hay chú ý đến:',
+        answerOptions: [
+            { answerText: 'Cảm nhận của bản thân. Đôi khi nó có thể hơi phóng đại.', answer_type: 'A' },
+            { answerText: 'Mô tả cụ thể. Đôi khi không chú ý đến độ dài cuộc trò chuyện.', answer_type: 'B' },
+            { answerText: 'Đạt được kết quả. Đôi khi có thể quá thẳng thắn khiến người khác khó chịu.', answer_type: 'C' },
+            { answerText: 'Cảm xúc giữa mọi người. Đôi khi không muốn nói ra sự thật.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Thông thường, nội tâm tôi luôn muốn:',
+        answerOptions: [
+            { answerText: 'Sự kích thích, thường có những ý tưởng mới, dám nghĩ dám làm, thích sự khác biệt.', answer_type: 'A' },
+            { answerText: 'Sự an toàn, đầu óc bình tĩnh, không dễ kích động.', answer_type: 'B' },
+            { answerText: 'Sự thử thách, trong cuộc sống thường xuất hiện cạnh tranh, khát khao “chiến thắng” rất mãnh liệt.', answer_type: 'C' },
+            { answerText: 'Sự ổn định, thỏa mãn với những gì mình đang có, hiếm khi ghen tị với người khác.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Đặc điểm cơ bản về mặt tình cảm của tôi là:',
+        answerOptions: [
+            { answerText: 'Cảm xúc luôn thay đổi, không ổn định.', answer_type: 'A' },
+            { answerText: 'Bề ngoài rất bình tĩnh, nhưng nội tâm lại có nhiều thay đổi, một khi đã bị tổn thương thì khó chữa lành.', answer_type: 'B' },
+            { answerText: 'Không dễ bộc lộ cảm xúc, nhưng khi cảm xúc bất ổn, thì rất dễ nổi nóng.', answer_type: 'C' },
+            { answerText: 'Cảm xúc luôn ổn định.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Về ham muốn kiểm soát, tôi cảm thấy:',
+        answerOptions: [
+            { answerText: 'Không có ham muốn kiểm soát, chỉ bị ảnh hưởng từ ham muốn của người khác, nhưng khả năng tự chủ của bản thân không đủ mạnh mẽ.', answer_type: 'A' },
+            { answerText: 'Duy trì kiểm soát bản thân và đặt ra yêu cầu với người khác bằng các quy tắc của riêng mình.', answer_type: 'B' },
+            { answerText: 'Có ham muốn kiểm soát và hy vọng rằng người khác sẽ nghe theo mình.', answer_type: 'C' },
+            { answerText: 'Không có hứng thú với việc kiểm soát người khác, cũng không muốn người khác kiểm soát mình.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Khi tiếp xúc với nửa kia, tôi mong rằng người ấy:',
+        answerOptions: [
+            { answerText: 'Thường xuyên khen ngợi tôi, khiến tôi hạnh phúc, quan tâm chăm sóc nhưng vẫn cho tôi tự do.', answer_type: 'A' },
+            { answerText: 'Có thể ngầm hiểu những gì tôi nghĩ bất cứ lúc nào, cực kỳ nhạy cảm với những mong muốn của tôi.', answer_type: 'B' },
+            { answerText: 'Được người ấy công nhận, tôi luôn đúng và tôi có giá trị đối với họ.', answer_type: 'C' },
+            { answerText: 'Tôn trọng và chung sống hòa bình với nhau.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Trong giao tiếp với người khác, tôi thường:',
+        answerOptions: [
+            { answerText: 'Nghĩ rằng giao lưu với mọi người sẽ vui hơn là ở một mình trong thời gian dài.', answer_type: 'A' },
+            { answerText: 'Tham gia vào cuộc trò chuyện một cách chậm rãi và dè dặt, nên thường khiến người khác có cảm giác xa cách.', answer_type: 'B' },
+            { answerText: 'Luôn muốn chiếm ưu thế trong các mối quan hệ.', answer_type: 'C' },
+            { answerText: 'Thuận theo tự nhiên, khá tẻ nhạt, tương đối thụ động.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Khi làm việc gì đó, tôi thường:',
+        answerOptions: [
+            { answerText: 'Thiếu sự lâu dài, không thích làm những việc giống nhau trong thời gian dài.', answer_type: 'A' },
+            { answerText: 'Thiếu sự quyết đoán, mong đợi kết quả tốt nhất, nhưng luôn nhìn thấy mặt tiêu cực của sự việc trước.', answer_type: 'B' },
+            { answerText: 'Thiếu sự kiên nhẫn, đôi khi hành động quá vội vàng.', answer_type: 'C' },
+            { answerText: 'Thiếu sự khẩn trương, hành động chậm chạp và khó hạ quyết tâm.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Thông thường, cách tôi hoàn thành công việc là:',
+        answerOptions: [
+            { answerText: 'Vào thời gian cuối cùng, gần deadline mới làm, là người chuyên để nước tới chân mới nhảy.', answer_type: 'A' },
+            { answerText: 'Bản thân tự đưa ra một kế hoạch khắt khe và nghiêm túc làm theo, không làm phiền đến ai.', answer_type: 'B' },
+            { answerText: 'Làm trước, làm nhanh.', answer_type: 'C' },
+            { answerText: 'Sử dụng phương pháp truyền thống, làm từng bước, lúc cần sẽ nhờ người khác giúp đỡ.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Khi ai đó làm tôi tổn thương sâu sắc, thì tôi:',
+        answerOptions: [
+            { answerText: 'Cảm thấy bị tổn thương và nghĩ rằng không thể tha thứ cho đối phương, nhưng cuối cùng, đa phần vẫn sẽ tha thứ.', answer_type: 'A' },
+            { answerText: 'Cảm thấy vô cùng tức giận, ghi nhớ điều đó và tránh mặt người đó trong tương lai.', answer_type: 'B' },
+            { answerText: 'Sẽ rất tức giận và mong đợi có cơ hội để đáp trả một cách gay gắt.', answer_type: 'C' },
+            { answerText: 'Tránh mâu thuẫn với người đó hoặc tự mình tìm kiếm những người bạn mới.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Trong các mối quan hệ, điều tôi quan tâm nhất là:',
+        answerOptions: [
+            { answerText: 'Được người khác khen ngợi, chào đón.', answer_type: 'A' },
+            { answerText: 'Được người khác hiểu và đánh giá cao.', answer_type: 'B' },
+            { answerText: 'Được người khác biết ơn và kính trọng.', answer_type: 'C' },
+            { answerText: 'Được người khác tôn trọng và chấp nhận.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Trong công việc, biểu hiện của tôi là:',
+        answerOptions: [
+            { answerText: 'Đầy nhiệt huyết, nhiều ý tưởng và rất có tinh thần.', answer_type: 'A' },
+            { answerText: 'Đầu óc tinh tế, hoàn hảo và đáng tin cậy.', answer_type: 'B' },
+            { answerText: 'Mạnh mẽ, thẳng thắn và có khả năng dẫn dắt người khác.', answer_type: 'C' },
+            { answerText: 'Kiên nhẫn, dễ thích nghi và phối hợp tốt với người khác.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Các giáo viên trước đây của tôi đã nhận xét tôi là người:',
+        answerOptions: [
+            { answerText: 'Giàu tình cảm, thích thể hiện và bộc lộ cảm xúc.', answer_type: 'A' },
+            { answerText: 'Thích sự riêng tư, đôi khi có vẻ cô đơn hoặc lạc lõng.', answer_type: 'B' },
+            { answerText: 'Nhanh nhẹn và độc lập, thích tự làm mọi việc.', answer_type: 'C' },
+            { answerText: 'Trông có vẻ trầm tĩnh và thong thả, tốc độ phản ứng chậm, tính cách ôn hòa.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Bạn bè thường đánh giá tôi là:',
+        answerOptions: [
+            { answerText: 'Thích kể mọi chuyện với bạn bè, dễ bị ảnh hưởng bởi người khác.', answer_type: 'A' },
+            { answerText: 'Có thể đặt nhiều câu hỏi cần sự tinh tế để giải đáp.', answer_type: 'B' },
+            { answerText: 'Thẳng thắn, đôi khi bàn luận thẳng thừng về những người hoặc sự việc mà mình không thích.', answer_type: 'C' },
+            { answerText: 'Thường là người biết lắng nghe người khác.', answer_type: 'D' },
+        ],
+    },
+    {
+        questionText: 'Về việc giúp đỡ người khác, tôi suy nghĩ:',
+        answerOptions: [
+            { answerText: 'Khi ai đó nhờ vả, tôi không mấy khi từ chối, thường cố gắng hết sức để giúp đỡ người ấy.', answer_type: 'A2' },
+            { answerText: 'Những người đáng được giúp đỡ thì nên giúp đỡ.', answer_type: 'B2' },
+            { answerText: 'Tôi hiếm khi hứa sẽ giúp đỡ ai, nhưng một khi đã hứa thì sẽ làm.', answer_type: 'C2' },
+            { answerText: 'Tuy không có tài năng gì xuất sắc, nhưng lại rất nhiệt tình giúp đỡ người khác.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Trước những lời khen ngợi của người khác dành cho mình, tôi cảm thấy:',
+        answerOptions: [
+            { answerText: 'Không có cũng chẳng sao cả, không mấy hứng thú với điều đó.', answer_type: 'A2' },
+            { answerText: 'Không cần những lời khen sáo rỗng, muốn đối phương đánh giá cao khả năng của mình.', answer_type: 'B2' },
+            { answerText: 'Suy nghĩ đối phương có thật lòng hay không hoặc lập tức tránh né sự chú ý của mọi người.', answer_type: 'C2' },
+            { answerText: 'Càng nhiều lời khen càng tốt, tôi rất thích điều đó.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Đối mặt với cuộc sống, tôi là:',
+        answerOptions: [
+            { answerText: 'Kiểu dễ tính – Không để tâm đến cái nhìn của người khác, cảm thấy bản thân như vậy là tốt rồi.', answer_type: 'A2' },
+            { answerText: 'Kiểu hành động - Nếu tôi không tiến bộ, người khác sẽ tiến bộ, vì vậy tôi phải liên tục phấn đấu.', answer_type: 'B2' },
+            { answerText: 'Kiểu phân tích - suy nghĩ về tất cả các khả năng phát sinh trước khi vấn đề xảy ra.', answer_type: 'C2' },
+            { answerText: 'Kiểu vô lo - điều quan trọng nhất là vui vẻ và hạnh phúc mỗi ngày.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Về các quy tắc, thái độ của tôi là:',
+        answerOptions: [
+            { answerText: 'Không muốn vi phạm quy tắc, nhưng có thể không đạt yêu cầu do quy tắc đưa ra vì bản thân khá thoải mái.', answer_type: 'A2' },
+            { answerText: 'Phá vỡ các quy tắc, hy vọng sẽ đặt ra các quy tắc thay vì tuân theo chúng.', answer_type: 'B2' },
+            { answerText: 'Nghiêm túc chấp hành các quy tắc và cố gắng làm tốt nhất.', answer_type: 'C2' },
+            { answerText: 'Tôi không thích bị ràng buộc bởi các quy tắc, không theo quy tắc sẽ cảm thấy mới mẻ và thú vị.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Tôi nghĩ những đặc điểm cơ bản trong cách làm việc của tôi là:',
+        answerOptions: [
+            { answerText: 'Thong thả ung dung, làm việc từng bước và phối hợp với những người xung quanh.', answer_type: 'A2' },
+            { answerText: 'Có mục tiêu rõ ràng, tập trung làm việc chăm chỉ và nắm bắt tốt những điểm cốt lõi để đạt được mục tiêu.', answer_type: 'B2' },
+            { answerText: 'Cẩn thận tỉ mỉ, vì đề phòng rủi ro và khắc phục hậu quả có thể dốc hết sức làm việc, không màng tất cả', answer_type: 'C2' },
+            { answerText: 'Nhanh nhẹn, năng động, không thích bị các quy định ràng buộc.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Khi tôi làm sai điều gì đó, tôi có xu hướng:',
+        answerOptions: [
+            { answerText: 'Sợ hãi nhưng không thể hiện ra.', answer_type: 'A2' },
+            { answerText: 'Không thừa nhận và phản bác lại, nhưng trong lòng đã ngầm hiểu.', answer_type: 'B2' },
+            { answerText: 'Cảm giác tội lỗi, dễ ở trong tình trạng tinh thần bị ức chế.', answer_type: 'C2' },
+            { answerText: 'Xấu hổ, hi vọng tránh được sự phê bình của người khác.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Khi kết thúc một mối quan hệ khó quên, tôi sẽ:',
+        answerOptions: [
+            { answerText: 'Rất khó chịu, nhưng cuộc sống vẫn phải tiếp diễn, thời gian sẽ làm mọi thứ phai nhạt.', answer_type: 'A2' },
+            { answerText: 'Dù tổn thương, nhưng một khi đã hạ quyết tâm, thì sẽ cố gắng hết sức để xóa bỏ bóng hình trong quá khứ.', answer_type: 'B2' },
+            { answerText: 'Trong một thời gian dài sẽ bị mắc kẹt trong tâm trạng buồn bã, không thể thoát ra được và không muốn mở lòng với người mới.', answer_type: 'C2' },
+            { answerText: 'Đau khổ đến mức không thiết sống, cần tìm một người bạn để nói chuyện hoặc tìm một nơi nào đó để trút giận.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Khi người khác tâm sự với mình, tôi thường có xu hướng:',
+        answerOptions: [
+            { answerText: 'Có thể nhìn nhận và thấu hiểu với cảm xúc của đối phương tại thời điểm đó.', answer_type: 'A2' },
+            { answerText: 'Nhanh chóng đưa ra một số kết luận hoặc phán đoán.', answer_type: 'B2' },
+            { answerText: 'Đưa ra một số phân tích hoặc suy luận, giúp đối phương nói thẳng ra suy nghĩ của họ.', answer_type: 'C2' },
+            { answerText: 'Đồng cảm với tâm trạng của đối phương, cũng có thể đưa ra một số nhận xét hoặc ý kiến.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Tôi thích trò chuyện với nhóm người nào dưới đây?',
+        answerOptions: [
+            { answerText: 'Trong không khí thoải mái, bình tĩnh và cuối cùng đi đến kết luận thống nhất.', answer_type: 'A2' },
+            { answerText: 'Mọi người tranh luận sôi nổi với nhau và thu hoạch được điều gì đó.', answer_type: 'B2' },
+            { answerText: 'Thảo luận về mặt tốt, mặt xấu và sức ảnh hưởng của sự việc một cách chi tiết và có ý nghĩa.', answer_type: 'C2' },
+            { answerText: 'Vui vẻ trò chuyện tự do, không bị gò bó.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Sâu thẳm trong lòng, tôi cảm thấy công việc:',
+        answerOptions: [
+            { answerText: 'Không nên có quá nhiều áp lực, có thể để tôi làm công việc mình quen thuộc là được rồi.', answer_type: 'A2' },
+            { answerText: 'Cần hoàn thành càng nhanh càng tốt, cố gắng hoàn thành nhiều nhiệm vụ hơn nữa.', answer_type: 'B2' },
+            { answerText: 'Không làm thì thôi nếu đã làm thì phải làm tốt nhất.', answer_type: 'C2' },
+            { answerText: 'Tốt nhất là nên có niềm vui khi làm việc, nếu tôi phải làm việc mình không thích thì thật nhàm chán.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Nếu tôi là lãnh đạo, tôi hy vọng rằng trong suy nghĩ của cấp dưới, tôi là người:',
+        answerOptions: [
+            { answerText: 'Dễ gần, hòa nhã và biết nghĩ cho người khác.', answer_type: 'A2' },
+            { answerText: 'Có năng lực lãnh đạo mạnh mẽ.', answer_type: 'B2' },
+            { answerText: 'Công bằng, chính trực và đáng tin cậy.', answer_type: 'C2' },
+            { answerText: 'Được mọi người yêu thích và có thể truyền cảm hứng cho người khác.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Yêu cầu của tôi về sự công nhận và tán đồng của mọi người là:',
+        answerOptions: [
+            { answerText: 'Cuộc sống vẫn phải tiếp diễn bất kể người khác có công nhận và tán đồng bạn hay không.', answer_type: 'A2' },
+            { answerText: 'Được những nhóm người ưu tú công nhận và tán đồng là điều quan trọng nhất.', answer_type: 'B2' },
+            { answerText: 'Chỉ cần những người tôi quan tâm công nhận và tán đồng là được rồi.', answer_type: 'C2' },
+            { answerText: 'Tất cả những người tôi gặp, bất kể giàu nghèo đều công nhận và tán đồng tôi, vậy thì tốt biết bao.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Khi tôi còn là một đứa trẻ, tôi:',
+        answerOptions: [
+            { answerText: 'Không tích cực thử những điều mới, thường thích những thứ cũ và quen thuộc.', answer_type: 'A2' },
+            { answerText: 'Cầm đầu đám trẻ con, tụi nhỏ thường nghe theo quyết định của tôi.', answer_type: 'B2' },
+            { answerText: 'Nhút nhát khi gặp người lạ, muốn trốn tránh.', answer_type: 'C2' },
+            { answerText: 'Nghịch ngợm, dễ thương, lạc quan và nhiệt tình.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Nếu tôi là cha mẹ, có thể tôi sẽ:',
+        answerOptions: [
+            { answerText: 'Dễ thuyết phục hoặc khoan dung.', answer_type: 'A2' },
+            { answerText: 'Nghiêm khắc, thiếu kiên nhẫn và quyết đoán.', answer_type: 'B2' },
+            { answerText: 'Khăng khăng làm theo suy nghĩ của mình và khá khó tính.', answer_type: 'C2' },
+            { answerText: 'Tích cực tham gia chơi cùng các con, được các con nhiệt liệt hoan nghênh.', answer_type: 'D2' },
+        ],
+    },
+    {
+        questionText: 'Câu châm ngôn phù hợp với cảm nhận của tôi nhất là:',
+        answerOptions: [
+            { answerText: 'Chân lí sâu sắc nhất là những gì đơn giản và bình thường nhất. Để thành công trong cuộc sống, phải biết khiêm tốn giấu tài. Trong xã hội này, tính cách tốt chính là trang phục đẹp nhất để khoác lên mình. Hạnh phúc lớn nhất trên thế gian là phải biết đủ.', answer_type: 'A2' },
+            { answerText: 'Đi trên con đường của riêng mình, mặc kệ người khác bàn tán. Cho dù thế giới đầy đau khổ, nhưng chúng ta luôn có thể vượt qua. Thành công là hạnh phúc chân chính duy nhất trong cuộc sống. Đối với tôi, giải quyết một vấn đề cũng vui vẻ như tận hưởng một kỳ nghỉ.', answer_type: 'B2' },
+            { answerText: 'Một người không chú ý đến những việc nhỏ, sẽ không bao giờ làm nên việc lớn. Lý trí là yếu tố cao quý nhất trong tâm hồn. Phải tránh khoác lác, phô trương. Thay vì phóng đại, chi bằng nói bớt đi. Thận trọng có tác dụng mạnh mẽ hơn nhiều so với sự táo bạo.', answer_type: 'C2' },
+            { answerText: 'Hạnh phúc nằm ở sự đam mê và niềm vui trong cuộc sống. Điều quan trọng nhất là luôn thành thật với chính mình. Biến cuộc sống thành tưởng tượng, lại biến tưởng tượng thành hiện thực. Hạnh phúc không nằm ở tiền tài, mà là niềm vui khi đạt được thành tựu và sự hưng phấn khi nảy sinh ý tưởng sáng tạo.', answer_type: 'D2' },
+        ],
+    },
+];
+export default question_data;
